9bec9d414da3044522220b47e776031948ff3eab
Peter Lebedev <LPeter@mail.ru>
2021-07-15 14:16:51 +0300

    new component - Categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Header from './components/Header.jsx'
-
+import { Header, Categories } from './components'
 
 function App() {
   return (
@@ -10,16 +9,10 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              items={["Мясные", "Вегетарианские", "Гриль", "Острые", "Закрытые"]}
+              onClick={(data) => alert(data)}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
