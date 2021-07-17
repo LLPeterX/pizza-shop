f11ebefeb350dd0926ca537902a022b5c5652ea6
Peter Lebedev <LPeter@mail.ru>
2021-07-17 13:52:46 +0300

    App: pass array for sort items

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <Categories
               items={["Мясные", "Вегетарианские", "Гриль", "Острые", "Закрытые"]}
             />
-            <SortPopup />
+            <SortPopup items={["популярности", "цене", "алфавиту"]} />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
