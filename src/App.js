533932b37dac15b3c2f460674ecb7ef76776d5b3
Peter Lebedev <LPeter@mail.ru>
2021-07-19 14:13:30 +0300

    test fetch from local file db.json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import { Header } from './components'
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then(resp => resp.json())
+      .then(data => setPizzas(data.pizzas));
+
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
+      <div>
+        {pizzas.length}
+        <ul>
+          {
+            pizzas.map(p => <li key={p.id}>{p.name}</li>)
+          }
+        </ul>
+      </div>
       <div className="content">
         <Route exact path="/" component={Home} />
         <Route exact path="/cart" component={Cart} />
