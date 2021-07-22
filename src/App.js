9d5fdd7f4e74a2c89507028d60427fd3fa1077b1
Peter Lebedev <LPeter@mail.ru>
2021-07-22 14:51:54 +0300

    apply redux to app - pizzas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import { Route } from 'react-router-dom'
 import { Header } from './components'
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+// actions
+import { setPizzas } from './redux/actions/pizzas'
 
 function App() {
 
-  const [pizzas, setPizzas] = useState([]);
-
+  console.log('render App');
+  //const [pizzas, setPizzas] = useState([]);
+  const dispatch = useDispatch();
+  const pizzasItems = useSelector(store => store.pizzas.items)
 
   useEffect(() => {
     axios.get('http://localhost:3000/db.json')
-      .then(({ data }) => setPizzas(data.pizzas));
-  }, []);
+      .then(({ data }) => dispatch(setPizzas(data.pizzas)));
+  }, [dispatch]);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" render={() => <Home pizzas={pizzas} />} />
+        <Route exact path="/" render={() => <Home pizzas={pizzasItems} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
