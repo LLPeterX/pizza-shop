bb9362d60487584409cc1b751ae9143cbfc6d1c6
Peter Lebedev <LPeter@mail.ru>
2021-07-29 13:54:36 +0300

    reducer 'ADD_CART' with calc totalCount & totalPrice

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,6 +1,6 @@
 const initalState = {
   items: {},
-  totalPrice: 1,
+  totalPrice: 0,
   totalCount: 0
 }
 
@@ -13,13 +13,21 @@ const cart = (state = initalState, action) => {
       return { ...state, totalCount: action.payload };
     case 'ADD_TO_CART':
       const id = action.payload.id;
-      return ({
+      const newItems = {
+        ...state.items,
+        [id]: !state.items[id] ? [action.payload] : [...state.items[id], action.payload]
+      };
+      // вычисление общего количества и цены
+      const itemsValues = Object.values(newItems).flat(); // массив массивов объектов пицц
+      const totalCount = itemsValues.length;
+      const totalPrice = itemsValues.reduce((sum, item) => sum + item.price, 0);
+      return {
         ...state,
-        items: {
-          ...state.items,
-          [id]: !state.items[id] || state.items[id].length === 0 ? [action.payload] : [...state.items[id], action.payload]
-        }
-      })
+        items: newItems,
+        // totalCount: state.totalCount + 1,
+        // totalPrice: state.totalPrice + action.payload.price
+        totalCount, totalPrice
+      }
     default:
       return state;
   }
