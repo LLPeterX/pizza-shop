c87b6650e63d4fc51879b876637d0bfbb54528af
Peter Lebedev <LPeter@mail.ru>
2021-08-02 12:06:32 +0300

    fix action ClearCart()

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,4 +1,4 @@
 export const addPizzaToCart = (pizza) => ({ type: 'ADD_TO_CART', payload: pizza });
 export const removePizzaFromCart = (pizza) => ({ type: 'REMOVE_FROM_CART', payload: pizza });
 export const deleteOrder = (pizza) => ({ type: 'DELETE_ORDER', payload: pizza });
-export const clearCart = (pizza) => ({ type: 'CLEAR_CART' });
+export const clearCart = () => ({ type: 'CLEAR_CART' });
